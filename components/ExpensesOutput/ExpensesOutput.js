2609059c494215b7562e9d61f247c76b5b056bc3
MorgynKass <morgynkass@gmail.com>
2024-08-21 13:33:03 -0600

    created expense context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,39 +4,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "Apple watch",
-    amount: 399.99,
-    date: new Date("2022-1-28"),
-  },
-  {
-    id: "e3",
-    description: "Shirt",
-    amount: 12.67,
-    date: new Date("2022-2-4"),
-  },
-  {
-    id: "e4",
-    description: "A pair of jeans",
-    amount: 24.99,
-    date: new Date("2022-2-16"),
-  },
-  {
-    id: "e5",
-    description: "Sunglasses",
-    amount: 59.99,
-    date: new Date("2022-2-21"),
-  },
-];
-
 function ExpensesOutput({ expenses, expenseInterval }) {
   return (
     <View style={styles.container}>
--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -15,11 +15,17 @@ function ManageExpenses({ route, navigation }) {
     });
   }, [isEditing, navigation]);
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
 
-  function cancelHandler() {}
+  function cancelHandler() {
+    navigation.goBack();
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
